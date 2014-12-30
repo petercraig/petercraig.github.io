ef3fbc283c1b10e84c1d94dcfe4941d551cb28f9
Peter Craig <peter-craig@live.co.uk>
2014-12-30 15:45:05 +0000

    petercraig

---
+++ craigventures.com/index.php
@@ -0,0 +1,94 @@
+<!-- team@craigventures.com -->
+
+<!DOCTYPE html>
+<html lang="en">
+    
+    <head>
+        
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="team@craigventures.com">
+        
+        <link rel="icon" href="img/favicon.ico">
+        
+        <title>Home &#8226; Craig Ventures</title>
+        
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+        <link href="assets/css/craigventures.css" rel="stylesheet">
+        
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    
+    </head>
+    
+    <body>
+        
+        <nav class="navbar navbar-inverse navbar-static-top">
+            
+            <div class="container">
+                
+                <div class="navbar-header"><button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false"aria-controls="navbar"><span class="sr-only">Toggle Navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="index.php">Craig Ventures</a></div>
+                
+                <div id="navbar" class="navbar-collapse collapse">
+                    
+                    <ul class="nav navbar-nav navbar-right">
+                        
+                        <li class="active"><a href="index.php"><i class="fa fa-home">&nbsp;</i>Home</a></li>
+                        <li><a href="about.php"><i class="fa fa-info-circle">&nbsp;</i>About</a></li>
+                        <li><a href="blog.php"><i class="fa fa-pencil-square-o">&nbsp;</i>Blog</a></li>
+                        
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" role="button"><i class="fa fa-folder-open">&nbsp;</i>Services</a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="services/website-design.php">Website Design</a></li>
+                                <li><a href="services/website-hosting.php">Website Hosting</a></li>
+                                <li><a href="services/ios-development.php">iOS Development</a></li>
+                                <li><a href="services/android-development.php">Android Development</a></li>
+                            </ul>
+                        </li>
+                        
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" role="button"><i class="fa fa-support">&nbsp;</i>Support</a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="http://board.craigventures.com/">Community Board</a></li>
+                                <li><a href="support/faq.php">Frequent Questions</a></li>
+                                <li><a href="support/contact.php">Contact Form</a></li>
+                            </ul>
+                        </li>
+                        
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" role="button"><i class="fa fa-user">&nbsp;</i>Account</a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#">Login</a></li>
+                                <li><a href="#">Register</a></li>
+                            </ul>
+                        </li>
+                    
+                    </ul>
+                
+                </div>
+            
+            </div>
+        </nav>
+        
+        <div class="container">
+            <div class="row">
+                
+                <div class="alert alert-danger" role="alert"><strong>Warning!</strong> We are currently undergoing scheduled maintenance. Please be aware our email servers are currently <u>offline</u> and mail will not be delivered.</div>
+                
+                </div>
+            </div>
+            
+        </div>
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="assets/js/craigventures.js"></script>
+    
+    </body>
+
+</html>
